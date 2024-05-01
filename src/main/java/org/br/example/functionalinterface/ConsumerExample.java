b4c9b533d3282963de38806c49003c8c51e792db
vzanbr <gabriecvr123@gmail.com>
2024-05-01 17:51:28 -0300

    Fix :  Consumer Example !

--- src/main/java/org/br/example/functionalinterface/ConsumerExample.java
+++ src/main/java/org/br/example/functionalinterface/ConsumerExample.java
@@ -12,7 +12,7 @@ import java.util.function.Consumer;
 public class ConsumerExample {
     public static void main(String[] args) {
         // Criar uma lista de números inteiros
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
 
         // Usar o Consumer com expressão lambda para imprimir números pares
         Consumer<Integer> imprimirNumeroPar = numero -> {
@@ -25,5 +25,6 @@ public class ConsumerExample {
         numeros.stream()
                 .filter(n -> n % 2 == 0)
                 .forEach(imprimirNumeroPar);
+//                .forEach(n -> System.out.println(n));
     }
 }
